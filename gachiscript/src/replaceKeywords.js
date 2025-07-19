678f24626f8827adbdc8784d2dcc54cff1fa93b1
Kr1tX12 <nion19255@gmail.com>
2025-07-19 18:49:48 +0300

    фвафывафывафывафыва

--- gachiscript/src/replaceKeywords.js
+++ gachiscript/src/replaceKeywords.js
@@ -1,23 +1,18 @@
 function replaceKeywords(code, dict) {
   return code.replace(/(["'`])(?:(?=(\\?))\2.)*?\1|\/\*[\s\S]*?\*\/|\/\/.*|[^\s]+/g, (match) => {
-    // Если это строка или комментарий — не трогаем
     if (
-      /^["'`]/.test(match) || // строка
-      /^\/\//.test(match) ||  // однострочный коммент
-      /^\/\*/.test(match)     // многострочный коммент
+      /^["'`]/.test(match) ||
+      /^\/\//.test(match) ||  
+      /^\/\*/.test(match)  
     ) {
       return match;
     }
-
-    // Чистим от лишних символов
     const key = match.trim();
 
-    // Если в словаре есть замена — заменяем
     if (dict[key]) {
       return dict[key];
     }
 
-    // Иначе возвращаем, как есть
     return match;
   });
 }
